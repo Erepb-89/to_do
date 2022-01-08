c90d1d59734f28f67428aa444261d8e6018cec9f
Erepb-89 <erepb-89@yandex.ru>
2022-01-08 17:24:58 +0300

    lesson12, added docker-compose and dockerfiles

--- todo/settings.py
+++ todo/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'todo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'frontend/build'],
+        'DIRS': [BASE_DIR / '../frontend/build'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -89,10 +89,21 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db',
+        'PASSWORD': '123456',
+        'USER': 'erepb',
+        'HOST': 'db',
+        'PORT': '5432'
     }
 }
 
@@ -131,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (BASE_DIR / 'frontend/build/static/',)
+STATICFILES_DIRS = (BASE_DIR / '../frontend/build/static/',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -145,7 +156,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        'rest_framework.permissions.AllowAny',
         # 'rest_framework.permissions.IsAuthenticated'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
--- users/management/commands/fill_db.py
+++ users/management/commands/fill_db.py
@@ -5,15 +5,17 @@ from users.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        User.objects.all().delete()
-        User.objects.create_superuser(username='erepb', password='1')
-        User.objects.create(username='admin', password='111', first_name='admin', last_name='adminoff',
-                            email='admin@yandex.ru')
-        User.objects.create(username='ivanoff', password='222', first_name='petr', last_name='ivanoff',
-                            email='ivanoff@yandex.ru')
-        User.objects.create(username='petroff', password='333', first_name='valiliy', last_name='petroff',
-                            email='petroff@yandex.ru')
-        User.objects.create(username='sidiroff', password='444', first_name='sergey', last_name='sidiroff',
-                            email='sidiroff@mail.ru')
-        User.objects.create(username='egoroff', password='555', first_name='ivan', last_name='egoroff',
-                            email='egoroff@mail.ru')
+        # User.objects.all().delete()
+        # User.objects.create_superuser(username='erepb', password='1')
+        # User.objects.create(username='admin', password='111', first_name='admin', last_name='adminoff',
+        #                     email='admin@yandex.ru')
+        # User.objects.create(username='ivanoff', password='222', first_name='petr', last_name='ivanoff',
+        #                     email='ivanoff@yandex.ru')
+        # User.objects.create(username='petroff', password='333', first_name='valiliy', last_name='petroff',
+        #                     email='petroff@yandex.ru')
+        # User.objects.create(username='sidiroff', password='444', first_name='sergey', last_name='sidiroff',
+        #                     email='sidiroff@mail.ru')
+        # User.objects.create(username='egoroff', password='555', first_name='ivan', last_name='egoroff',
+        #                     email='egoroff@mail.ru')
+        User.objects.create(username='test', password='123', first_name='test', last_name='testoff',
+                            email='test@mail.ru')
